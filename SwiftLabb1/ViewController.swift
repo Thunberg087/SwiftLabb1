1ec2abdcb47ae7d1c55dc25fea312066ffa9f1fa
Jonathan Thunberg <thunberg00@gmail.com>
2020-02-03 11:14:15 +0100

    Initial Commit

---
+++ SwiftLabb1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLabb1
+//
+//  Created by Jonathan Thunberg on 2020-02-03.
+//  Copyright © 2020 Jonathan Thunberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
